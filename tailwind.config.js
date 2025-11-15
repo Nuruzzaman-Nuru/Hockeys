96a31f48ae4e558c6a7fe9be0957d381883e7778
Nuruzzaman-Nuru <nnuru8805@gmail.com>
2025-11-16 01:02:53 +0600

    new

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Manrope', 'ui-sans-serif', 'system-ui']
+                    },
+                    colors: {
+                        brand: {
+                            DEFAULT: '#FF4240',
+                            dark: '#DB2D2C'
+                        }
+                    },
+                    boxShadow: {
+                        soft: '0 25px 50px -12px rgba(0, 0, 0, 0.08)'
+                    }
+                }
+            }
+        }
